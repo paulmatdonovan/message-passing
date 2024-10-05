dd71b0624fefeb3f78b57749d68558a76bbf3773
Paul Donovan <paul.m.donovan@outlook.com>
2024-10-05 20:24:57 +1000

    completes project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function () {
 
-    const form = document.querySelector("message-form");
+    const form = document.querySelector("#message-form");
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
@@ -9,13 +9,16 @@
         // class requires .feedback 
         const feedback = document.querySelector('.feedback');
         const messageContent = document.querySelector('.message-content');
+
+
+        if (message.value === '') {
+            feedback.classList.add('show')
+            setTimeout(function () {
+                feedback.classList.remove('show')
+            }, 4000)
+        } else {
+            messageContent.textContent = message.value
+            message.value = ''
+        }
     })
-
-    if (message.value === '') {
-        feedback.classList.add('show')
-        setTimeout(function () {
-            feedback.classList.remove('show')
-        }, 4000)
-    }
-
-})
+})()
