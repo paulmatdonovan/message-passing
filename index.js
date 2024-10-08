ee1f4687d3282b3bf8c01e5f7fd97d61f757edec
Paul Donovan <paul.m.donovan@outlook.com>
2024-10-08 19:30:21 +1100

    finished app

--- index.js
+++ index.js
@@ -5,11 +5,20 @@
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         const message = document.querySelector('#message');
+        const feedback = document.querySelector('.feedback')
         const messageContent = document.querySelector('.message-content')
 
-        messageContent.textContent = message.value
-        message.value = ''
 
+        if (message.value === '') {
+            feedback.classList.add('show')
+            setTimeout(function () {
+                feedback.classList.remove('show')
+            }, 4000)
+        } else {
+            messageContent.textContent = message.value
+            message.value = ''
+
+        }
     })
 })()
 
